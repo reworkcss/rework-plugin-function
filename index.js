c041cbde64f911e90808579bed26950a512cf633
Eric Fong <ericff@gmail.com>
2015-02-12 10:06:01 +0800

    use return instead

--- index.js
+++ index.js
@@ -31,45 +31,43 @@ module.exports = function(functions, args) {
  */
 
 function func(declarations, functions, functionMatcher, parseArgs) {
+  // ISSUE#2: sass gen comments cause error
+  if (!declarations) return;
   if (false !== parseArgs) parseArgs = true;
-
-  if (declarations) {
+  
+  declarations.forEach(function(decl){
+    if ('comment' == decl.type) return;
+    var generatedFuncs = [], result, generatedFunc;
     
-    declarations.forEach(function(decl){
-      if ('comment' == decl.type) return;
-      var generatedFuncs = [], result, generatedFunc;
-      
-      while (decl.value.match(functionMatcher)) {
-        decl.value = decl.value.replace(functionMatcher, function(_, name, args){
-          if (parseArgs) {
-            args = args.split(/\s*,\s*/).map(strip);
-          } else {
-            args = [strip(args)];
-          }
-          // Ensure result is string
-          result = '' + functions[name].apply(decl, args);
-          
-          // Prevent fall into infinite loop like this:
-          //
-          // {
-          //   url: function(path) {
-          //     return 'url(' + '/some/prefix' + path + ')'
-          //   }
-          // }
-          //
-          generatedFunc = {from: name, to: name + getRandomString()};
-          result = result.replace(functionMatcherBuilder(name), generatedFunc.to + '($2)');
-          generatedFuncs.push(generatedFunc);
-          return result;
-        });
-      }
-      
-      generatedFuncs.forEach(function(func) {
-        decl.value = decl.value.replace(func.to, func.from);
-      })
-    });
+    while (decl.value.match(functionMatcher)) {
+      decl.value = decl.value.replace(functionMatcher, function(_, name, args){
+        if (parseArgs) {
+          args = args.split(/\s*,\s*/).map(strip);
+        } else {
+          args = [strip(args)];
+        }
+        // Ensure result is string
+        result = '' + functions[name].apply(decl, args);
+        
+        // Prevent fall into infinite loop like this:
+        //
+        // {
+        //   url: function(path) {
+        //     return 'url(' + '/some/prefix' + path + ')'
+        //   }
+        // }
+        //
+        generatedFunc = {from: name, to: name + getRandomString()};
+        result = result.replace(functionMatcherBuilder(name), generatedFunc.to + '($2)');
+        generatedFuncs.push(generatedFunc);
+        return result;
+      });
+    }
     
-  }
+    generatedFuncs.forEach(function(func) {
+      decl.value = decl.value.replace(func.to, func.from);
+    })
+  });
 
 }
 
